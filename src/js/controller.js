fe96762425c34db584e77522ace6212c97a5255e
Jirka <jiri.suk.8@seznam.cz>
2021-11-10 19:36:08 +0200

    Cleaned controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -78,7 +78,6 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   //update recipe view
-  // recipeView.render(model.state.recipe);
   recipeView.update(model.state.recipe);
 };
 
