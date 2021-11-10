e296bac6f3c56481b09b792e9a09cfe7b0b11a07
Jirka <jiri.suk.8@seznam.cz>
2021-11-10 19:21:39 +0200

    final

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -9,6 +9,28 @@ const timeout = function (s) {
   });
 };
 
+export const AJAX = async function (url, uploadData = undefined) {
+  try {
+    const fetchPro = uploadData
+      ? fetch(url, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(uploadData),
+        })
+      : fetch(url);
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    //propaguju error o uroven niz
+    // aby se ten predchozi promise nevyplnil hodim znova error
+    throw err;
+  }
+};
+
+/*
 export const getJSON = async function (url) {
   try {
     const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
@@ -42,4 +64,4 @@ export const sendJSON = async function (url, uploadData) {
     // aby se ten predchozi promise nevyplnil hodim znova error
     throw err;
   }
-};
+}; */
